dd38baffea56c52e50881048d818805eb48970c3
Elliott-Greaves <greaveselliott@me.com>
2017-05-03 22:33:40 +0100

    Added redux-devtools-exension configuration

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,6 +1,7 @@
 import rootReducer from '../reducers';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 };
